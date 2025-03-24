e34f4fa550b0c66739f2040729f7affd87fa62cf
Davin Alfarizky Putra Basudewa <dbasudewa@gmail.com>
2025-03-24 07:36:39 +0700

    chore: Disable enable wayland

--- config/appearance.lua
+++ config/appearance.lua
@@ -5,6 +5,7 @@ local colors = require('colors.custom')
 return {
    max_fps = 120,
    front_end = 'WebGpu',
+   enable_wayland = false,
    webgpu_power_preference = 'HighPerformance',
    webgpu_preferred_adapter = gpu_adapters:pick_best(),
    -- webgpu_preferred_adapter = gpu_adapters:pick_manual('Dx12', 'IntegratedGpu'),
